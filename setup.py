d7f5eee2db71185590c361936e1f231692f6baa8
xinyu <hexinyu@lingkou.com>
2020-09-04 21:47:15 +0800

    fix: setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 
 setup(
+    name="ktlint",
+    version="0.0.0",
+    packages=["lint"],
     entry_points={
         'console_scripts': [
             'ktlint = lint.ktlint:run_ktlint',
