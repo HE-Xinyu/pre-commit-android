00cbd0b3f938600dc5a30b3cf9689d3057449526
xinyu <hexinyu@lingkou.com>
2020-09-04 21:49:47 +0800

    fix: deps in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="ktlint",
     version="0.0.0",
     packages=["lint"],
+    install_requires=["requests"],
     entry_points={
         'console_scripts': [
             'ktlint = lint.ktlint:run_ktlint',
